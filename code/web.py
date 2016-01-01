6d51f5d6ee98ea25ac95a14a5f25d90b3889ff8b
Edward Cree <ec429@cantab.net>
2016-01-01 05:37:42 +0000

    Update Web frontend to handle Asteroids.  Then comment it out
    The image generator doesn't support setting the MainBody yet.

--- code/web.py
+++ code/web.py
@@ -10,31 +10,54 @@ from twisted.web import server, resource
 from twisted.internet import reactor, endpoints
 
 def gen_checks(b, merits):
-    devices = []
+    groups = []
+    soi_string = 'Reached SOI'
     if isinstance(b, ribbonator.Asteroid):
-        return '' # Not supported yet!
+        soi_string = 'Visited (within 2.2km)'
+        if 0: # MainBody not yet supported by the generator backend
+            group = []
+            for mb in ribbonator.bodies:
+                if isinstance(mb, (ribbonator.Star, ribbonator.Planet)):
+                    v = 'checked' if 'mb_' + mb.name in merits else None
+                    group.extend((t.input(type='radio', name='%s_mainbody'%(b.name,), value=mb.name, checked=v), mb.name))
+            groups.append(t.fieldset[t.legend['MainBody'],
+                                     group,
+                                     t.input(type='radio', name='%s_mainbody'%(b.name), value=''), 'Clear',
+                                     ])
+    lhgroup = []
     for d in 'oeprgnAX': # left-hand devices
         if b.permit(d):
             v = 'checked' if d in merits else None
-            devices.append(t.li[t.input(type='checkbox', name='%s_%s'%(b.name, d), checked=v), ribbonator.devices[d]])
+            lhgroup.extend((t.input(type='checkbox', name='%s_%s'%(b.name, d), checked=v), ribbonator.devices[d]))
+    groups.append(t.fieldset[t.legend['Left-Hand Devices'], lhgroup])
+    rhgroup = []
     for c in sorted(ribbonator.craft_devices, key=lambda c:ribbonator.devices[c]): # right-hand devices
         if b.permit(c):
             v = 'checked' if c in merits else None
-            devices.append(t.li[t.input(type='radio', name='%s_craft'%(b.name,), value=c, checked=v), ribbonator.devices[c]])
+            rhgroup.extend((t.input(type='radio', name='%s_craft'%(b.name,), value=c, checked=v), ribbonator.devices[c]))
+    groups.append(t.fieldset[t.legend['Right-Hand ("Craft") Devices'],
+                             rhgroup,
+                             t.input(type='radio', name='%s_craft'%(b.name), value=''), 'Clear',
+                             ])
+    other = []
     for d in '?|+^':
         if b.permit(d):
             v = 'checked' if d in merits else None
-            devices.append(t.li[t.input(type='checkbox', name='%s_%s'%(b.name, d), checked=v), ribbonator.devices[d]])
+            other.extend((t.input(type='checkbox', name='%s_%s'%(b.name, d), checked=v), ribbonator.devices[d]))
     if b.permit('W'):
         v = 'checked' if 'W' in merits else None
-        devices.append(t.li[t.input(type='checkbox', name='%s_W'%(b.name,), checked=v), 'Challenge Wreath: ', b.wreath])
+        other.extend((t.input(type='checkbox', name='%s_W'%(b.name,), checked=v), 'Challenge Wreath: ', b.wreath))
     v = 'checked' if 'soi' in merits else None
+    groups.append(t.fieldset[t.legend['Other Devices'], other])
     return t.fieldset[t.legend[b.name],
-                      t.input(type='checkbox', name='%s_soi'%(b.name,), checked=v), 'Reached SOI',
-                      t.ul[devices],
+                      t.input(type='checkbox', name='%s_soi'%(b.name,), checked=v), soi_string,
+                      groups,
                       ]
 
 def gen_job(b, merits):
+    if any('mb_' in m for m in merits):
+        mb = [m for m in merits if m.startswith('mb_')][0][3:]
+        b = '%s-%s'%(b, mb)
     return '%s=%s'%(b, ''.join(m for m in merits if len(m) == 1))
 
 def parse_merits(kwargs):
@@ -45,6 +68,9 @@ def parse_merits(kwargs):
         merits.setdefault(b, [])
         if c == 'craft':
             merits[b].append(v)
+        elif c == 'mainbody':
+            if v:
+                merits[b].append('mb_'+v)
         elif c:
             merits[b].append(c)
         else: # Job Card format
@@ -60,7 +86,7 @@ def page_body(kwargs):
             t.p["Generator and RSS Ribbons by Edward Cree.  Based on the KSP Ribbons by Unistrut.  'Inspired' by ", t.a(href='http://www.kerbaltek.com/ribbons')["Ezriilc's Ribbon Generator"], "."],
             t.p["Select your achievements with the checkboxes and radiobuttons, and click Submit to generate the ribbon image URL.  This will also generate a Ribbonator 'job card' URL; bookmark this if you want to be able to update your ribbons later."],
             t.p["The Ribbonator does not store any user data.  Instead, the ribbon contents are encoded in the URL of the image, using the same 'job card' format."],
-            t.p["Note: Asteroid ribbons are not supported yet."],
+            t.p["Note: Asteroid ribbon doesn't yet allow setting MainBody.  This is because the generator backend doesn't support any MainBody other than Earth yet."],
             t.img(src='gen.png'+job, alt="Generated ribbons"),
             t.p[t.a(href=job)["Job Card URL - bookmark this"]],
             t.form(method='GET')[t.ul[checks],
